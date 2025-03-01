17f84dca05c117b102ae494b97d676d1cc94c85c
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-03-01 10:51:54 -0300

    feat: first commit

---
+++ src/main/java/br/com/payment_integrator/PaymentIntegratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.payment_integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/payment_integrator/PaymentIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.payment_integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
