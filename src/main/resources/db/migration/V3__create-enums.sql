6aee49e6388087339fa061bfa587e6bb78e2f8ce
Lucas Dantas <lucas.dantas@voxtec.com.br>
2025-03-03 23:17:12 -0300

    feat: add new enum and new fields in payment

--- src/main/resources/db/migration/V3__create-enums.sql
+++ src/main/resources/db/migration/V3__create-enums.sql
@@ -1,2 +1,3 @@
 CREATE TYPE status_payment AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
 CREATE TYPE status_transaction AS ENUM ('SUCCESS', 'FAILED');
+CREATE TYPE payment_method AS ENUM ('pix', 'boleto', 'credit_card');
--- src/main/resources/db/migration/V4__create-users-and-payments.sql
+++ src/main/resources/db/migration/V4__create-users-and-payments.sql
@@ -11,7 +11,9 @@ CREATE TABLE IF NOT EXISTS financial.tb_payment (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     user_id UUID REFERENCES authentication.tb_user(id),
     amount DECIMAL(10,2) NOT NULL,
+    currency VARCHAR(3) NOT NULL DEFAULT 'BRL',
     status STATUS_PAYMENT NOT NULL,
+    payment_method PAYMENT_METHOD NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_status_at TIMESTAMP
 );
