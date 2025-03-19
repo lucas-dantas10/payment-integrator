902c5a7d21d864fe83cce617d49bc884d2b13573
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-03-19 20:29:40 -0300

    fix: adjust in validations of name and email

--- src/main/java/br/com/payment_integrator/domain/dto/payment/request/create_payment/CustomerDTO.java
+++ src/main/java/br/com/payment_integrator/domain/dto/payment/request/create_payment/CustomerDTO.java
@@ -3,15 +3,15 @@ package br.com.payment_integrator.domain.dto.payment.request.create_payment;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CustomerDTO(
         @NotNull(message = "O campo de name é obrigatório")
-        @Min(value = 1, message = "O nome precisa ter pelo menos 3 caracteres")
-        @Max(value = 200, message = "O nome espera no máximo 200 caracteres")
+        @Size(min = 3, max = 200, message = "O nome precisa ter entre 3 e 200 caracteres")
         String name,
 
         @NotNull(message = "O campo de email é obrigatório")
-        @Max(value = 200, message = "O email espera no máximo 200 caracteres")
+        @Size(min = 3, max = 200, message = "O email precisa ter entre 3 e 200 caracteres")
         String email
 ) {
 }
--- src/main/java/br/com/payment_integrator/domain/dto/payment/request/create_payment/ProductDTO.java
+++ src/main/java/br/com/payment_integrator/domain/dto/payment/request/create_payment/ProductDTO.java
@@ -3,17 +3,16 @@ package br.com.payment_integrator.domain.dto.payment.request.create_payment;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
 public record ProductDTO(
         @NotNull(message = "O campo de nome é obrigatório")
-        @Min(value = 1, message = "O nome precisa ter pelo menos 3 caracteres")
-        @Max(value = 200, message = "O nome espera no máximo 200 caracteres")
+        @Size(min = 3, max = 200, message = "O nome precisa ter entre 3 e 200 caracteres")
         String name,
 
-        @Min(value = 1, message = "A descrição precisa ter pelo menos 3 caracteres")
-        @Max(value = 300, message = "A descrição espera no máximo 300 caracteres")
+        @Size(min = 3, max = 200, message = "A descrição precisa ter entre 3 e 200 caracteres")
         String description,
 
         @NotNull(message = "O campo de price é obrigatório")
