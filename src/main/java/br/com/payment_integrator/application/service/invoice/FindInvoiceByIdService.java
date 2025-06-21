e57c67f45fe046afdf1d438e951fec976a7a1c6a
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2025-06-21 16:05:58 -0300

    feat: add search invoice by account and invoice id

--- src/main/java/br/com/payment_integrator/application/service/invoice/FindInvoiceByIdService.java
+++ src/main/java/br/com/payment_integrator/application/service/invoice/FindInvoiceByIdService.java
@@ -1,7 +1,9 @@
 package br.com.payment_integrator.application.service.invoice;
 
+import br.com.payment_integrator.application.util.AuthenticatedAccount;
 import br.com.payment_integrator.domain.builder.invoice.InvoiceResponseBuilder;
 import br.com.payment_integrator.domain.dto.invoice.response.FindInvoiceById.InvoiceDetailsResponseDTO;
+import br.com.payment_integrator.domain.entity.authentication.Account;
 import br.com.payment_integrator.domain.entity.financial.Invoice;
 import br.com.payment_integrator.domain.exception.invoice.InvoiceNotFoundException;
 import br.com.payment_integrator.domain.service.invoice.IFindInvoiceByIdService;
@@ -20,7 +22,8 @@ public class FindInvoiceByIdService implements IFindInvoiceByIdService {
 
     @Override
     public InvoiceDetailsResponseDTO execute(String id) {
-        Invoice invoice = invoiceRepository.findById(UUID.fromString(id)).orElseThrow(InvoiceNotFoundException::new);
+        Account account = AuthenticatedAccount.get();
+        Invoice invoice = invoiceRepository.findByIdAndAccount(UUID.fromString(id), account).orElseThrow(InvoiceNotFoundException::new);
 
         return invoiceResponseBuilder.build(invoice);
     }
--- src/main/java/br/com/payment_integrator/infra/repository/financial/InvoiceRepository.java
+++ src/main/java/br/com/payment_integrator/infra/repository/financial/InvoiceRepository.java
@@ -1,11 +1,14 @@
 package br.com.payment_integrator.infra.repository.financial;
 
+import br.com.payment_integrator.domain.entity.authentication.Account;
 import br.com.payment_integrator.domain.entity.financial.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
+    Optional<Invoice> findByIdAndAccount(UUID id, Account account);
 }
